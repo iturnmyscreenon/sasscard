3d0e800f5f56d27b9bf336568e7dcdbecde1845b
iturnmyscreenon <benoit.galey@gmail.com>
2022-11-15 12:29:57 +0100

    made card with vite and sass

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./App.scss";
+import jouer from "./assets/jouer.png";
+import backward from "./assets/backward-arrows-couple.png";
+import forward from "./assets/fast-forward.png";
+import booba from "./assets/booba-dkr-clip.jpg";
+
+function App() {
+  const [isClicked, setIsClicked] = useState(false);
+  return (
+    <section className="container">
+      <div className="card">
+        <img src={booba} alt="booba" />
+        <div className="card-head">
+          <div className="card-info">
+            <h1 className="card-title">DKR</h1>
+            <h2 className="card-singer">Booba</h2>
+
+          </div>
+          <div type="button" onClick={() => setIsClicked(!isClicked)}>
+          </div>
+        </div>
+        <div className="playlist-navigation">
+          <div className="progress-bar">
+            <div className="progress"></div>
+          </div>
+          <div className="music-timestamps">
+            <p className="music-timestamp">0:00</p>
+            <p className="music-timestamp">4:19</p>
+          </div>
+          <div className="navigation-menu">
+            <img src={backward} alt="backward" />
+          <img className="jouer" src={jouer}></img>
+            <img src={forward} alt="forward" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
